a08401aebd6e62e4abf0942b2bc04dcd535f10fb
Александр Пятугин <aleksandr.pyatugin@gost-group.com>
2021-02-15 11:39:37 +0300

    added themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,67 @@
-import { ValueFormatterParams } from 'ag-grid-community';
-import React from 'react';
-import { ActionElement } from './ActionElement';
-import { CustomHeader } from './CustomHeader';
+import { ValueFormatterParams } from "ag-grid-community";
+import React from "react";
+import { ActionElement } from "./ActionElement";
+import { CustomHeader } from "./CustomHeader";
 
-import { RandomPageWithAPropsAndXhr } from './ready-table';
+import { RandomPageWithAPropsAndXhr } from "./ready-table";
 
 import {
-    ColumnBuilder,
-    Table,
-    TableBuilder,
-    TableBuilderImplementation,
-    TableComponent,
-    ColumnBuilderImplementation,
-    Params,
-    ActionColumnBuilder,
-    ActionColumnBuilderImplementation,
-} from './table-builder';
+  ColumnBuilder,
+  Table,
+  TableBuilder,
+  TableBuilderImplementation,
+  TableComponent,
+  ColumnBuilderImplementation,
+  Params,
+  ActionColumnBuilder,
+  ActionColumnBuilderImplementation,
+} from "./table-builder";
 
+import "./ag-commont-styles.css";
 
 const App = () => {
-    const tableBuilder: TableBuilder = new TableBuilderImplementation();
+  const tableBuilder: TableBuilder = new TableBuilderImplementation();
 
-    tableBuilder.registerComponent('actions', ActionElement);
-    tableBuilder.registerComponent('customHeader', CustomHeader);
+  tableBuilder.registerComponent("actions", ActionElement);
+  tableBuilder.registerComponent("customHeader", CustomHeader);
 
-    const checkbox: ColumnBuilder = new ColumnBuilderImplementation();
-    checkbox.setCheckbox((target: Params<any>) => {
-        // here can be any filtered function
-        return target.data.price > 35000;
-    });
+  const checkbox: ColumnBuilder = new ColumnBuilderImplementation();
+  checkbox.setCheckbox((target: Params<any>) => {
+    // here can be any filtered function
+    return target.data.price > 35000;
+  });
 
-    const make: ColumnBuilder = new ColumnBuilderImplementation();
-    make.setField('make');
-    make.setHeader('Fancy header');
+  const make: ColumnBuilder = new ColumnBuilderImplementation();
+  make.setField("make");
+  make.setHeader("Fancy header");
 
-    const model: ColumnBuilder = new ColumnBuilderImplementation();
-    model.setField('model');
-    model.setEditable();
+  const model: ColumnBuilder = new ColumnBuilderImplementation();
+  model.setField("model");
+  model.setEditable();
 
-    const price: ColumnBuilder = new ColumnBuilderImplementation();
-    price.setField('price');
-    price.setSortable();
-    price.setValueFormatter((target: ValueFormatterParams): string => {
-        return '$ ' + target.value;
-    });
+  const price: ColumnBuilder = new ColumnBuilderImplementation();
+  price.setField("price");
+  price.setSortable();
+  price.setValueFormatter((target: ValueFormatterParams): string => {
+    return "$ " + target.value;
+  });
 
-    const actions: ActionColumnBuilder = new ActionColumnBuilderImplementation();
-    actions.setHeader('Actions');
-    actions.setRenderer('actions');
-    actions.setCustomHeader('customHeader');
+  const actions: ActionColumnBuilder = new ActionColumnBuilderImplementation();
+  actions.setHeader("Actions");
+  actions.setRenderer("actions");
+  actions.setCustomHeader("customHeader");
 
-    const table: Table = tableBuilder
-        .addColumn(checkbox.buildColumn())
-        .addColumn(make.buildColumn())
-        .addColumn(model.buildColumn())
-        .addColumn(price.buildColumn())
-        .addColumn(actions.buildColumn())
-        .buildTable();
+  const table: Table = tableBuilder
+    .addColumn(checkbox.buildColumn())
+    .addColumn(make.buildColumn())
+    .addColumn(model.buildColumn())
+    .addColumn(price.buildColumn())
+    .addColumn(actions.buildColumn())
+    .buildTable();
 
-    const TableElement: TableComponent = table.getReadyToUseJSXElement();
+  const TableElement: TableComponent = table.getReadyToUseJSXElement();
 
-    return (
-        <RandomPageWithAPropsAndXhr Table={TableElement} />
-    );
+  return <RandomPageWithAPropsAndXhr Table={TableElement} />;
 };
 
 export default App;
